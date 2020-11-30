c83711bf22267fe4e74d252012b0d6e5e8fb5566
Han Yang <63957848+hanyang0@users.noreply.github.com>
2020-11-30 23:13:16 +0800

    Add files via upload

---
+++ power/power/Form1.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox_H = New System.Windows.Forms.TextBox()
+        Me.TextBox_B = New System.Windows.Forms.TextBox()
+        Me.TextBox_L = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.LightBlue
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(450, 250)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "坐标系"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.Location = New System.Drawing.Point(28, 192)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Padding = New System.Windows.Forms.Padding(50, 0, 50, 0)
+        Me.RadioButton4.Size = New System.Drawing.Size(400, 50)
+        Me.RadioButton4.TabIndex = 0
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Krassovsky"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.Location = New System.Drawing.Point(28, 136)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Padding = New System.Windows.Forms.Padding(50, 0, 50, 0)
+        Me.RadioButton3.Size = New System.Drawing.Size(400, 50)
+        Me.RadioButton3.TabIndex = 0
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "IUGG75"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.Location = New System.Drawing.Point(28, 80)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Padding = New System.Windows.Forms.Padding(50, 0, 50, 0)
+        Me.RadioButton2.Size = New System.Drawing.Size(400, 50)
+        Me.RadioButton2.TabIndex = 0
+        Me.RadioButton2.Text = "WGS84"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(28, 24)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Padding = New System.Windows.Forms.Padding(50, 0, 50, 0)
+        Me.RadioButton1.Size = New System.Drawing.Size(400, 50)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "CGCS2000"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox_H)
+        Me.GroupBox2.Controls.Add(Me.TextBox_B)
+        Me.GroupBox2.Controls.Add(Me.TextBox_L)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(30, 318)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(450, 208)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "输入"
+        '
+        'TextBox_H
+        '
+        Me.TextBox_H.BackColor = System.Drawing.Color.LightBlue
+        Me.TextBox_H.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox_H.Location = New System.Drawing.Point(134, 163)
+        Me.TextBox_H.Name = "TextBox_H"
+        Me.TextBox_H.Size = New System.Drawing.Size(200, 25)
+        Me.TextBox_H.TabIndex = 1
+        '
+        'TextBox_B
+        '
+        Me.TextBox_B.BackColor = System.Drawing.Color.LightBlue
+        Me.TextBox_B.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox_B.Location = New System.Drawing.Point(134, 91)
+        Me.TextBox_B.Name = "TextBox_B"
+        Me.TextBox_B.Size = New System.Drawing.Size(200, 25)
+        Me.TextBox_B.TabIndex = 1
+        '
+        'TextBox_L
+        '
+        Me.TextBox_L.BackColor = System.Drawing.Color.LightBlue
+        Me.TextBox_L.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox_L.Location = New System.Drawing.Point(134, 24)
+        Me.TextBox_L.Name = "TextBox_L"
+        Me.TextBox_L.Size = New System.Drawing.Size(200, 25)
+        Me.TextBox_L.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(28, 163)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 25)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "H："
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(28, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 25)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "B："
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.Location = New System.Drawing.Point(344, 163)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Padding = New System.Windows.Forms.Padding(20, 0, 20, 0)
+        Me.Label7.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label7.Size = New System.Drawing.Size(100, 25)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "M"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(340, 91)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(100, 25)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "D.MMSS"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(340, 24)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 25)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "D.MMSS"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(28, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "L："
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.LightBlue
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox4.Location = New System.Drawing.Point(39, 38)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(390, 440)
+        Me.TextBox4.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button1.Location = New System.Drawing.Point(525, 551)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(456, 80)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "计算"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox4)
+        Me.GroupBox3.Location = New System.Drawing.Point(525, 28)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(456, 498)
+        Me.GroupBox3.TabIndex = 5
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "输出"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.RadioButton6)
+        Me.GroupBox4.Controls.Add(Me.RadioButton5)
+        Me.GroupBox4.Cursor = System.Windows.Forms.Cursors.Default
+        Me.GroupBox4.Location = New System.Drawing.Point(30, 551)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(450, 80)
+        Me.GroupBox4.TabIndex = 6
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "命令"
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.Location = New System.Drawing.Point(225, 24)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Padding = New System.Windows.Forms.Padding(50, 0, 50, 0)
+        Me.RadioButton6.Size = New System.Drawing.Size(225, 50)
+        Me.RadioButton6.TabIndex = 0
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "XYZ to LBH"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.Checked = True
+        Me.RadioButton5.Location = New System.Drawing.Point(0, 24)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Padding = New System.Windows.Forms.Padding(50, 0, 50, 0)
+        Me.RadioButton5.Size = New System.Drawing.Size(225, 50)
+        Me.RadioButton5.TabIndex = 0
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "LBH to XYZ"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightBlue
+        Me.ClientSize = New System.Drawing.Size(1006, 643)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "坐标转换"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox_H As TextBox
+    Friend WithEvents TextBox_B As TextBox
+    Friend WithEvents TextBox_L As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+End Class
---
+++ power/power/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.Math
+Public Class Form1
+    Function CRad(ByVal DMS As Double) As Double
+        Dim valuesign As Integer
+        Dim DD As Integer
+        Dim MM As Integer
+        Dim SS As Single
+        Dim DEG As Double
+        valuesign = Sign(DMS)
+        DMS = DMS * valuesign
+        DD = Int(DMS)
+        MM = Int(((DMS) - DD) * 100)
+        SS = ((DMS - DD) * 100 - MM) * 100
+        DEG = DD + MM / 60 + SS / 3600
+        Return (DEG * PI / 180) * valuesign
+    End Function
+
+    Function CMDS(ByVal Rad As Double) As String
+        Dim valuesign As Integer
+        Dim DD As Integer
+        Dim MM As Integer
+        Dim SS As Single
+        Dim DEG As Double
+        valuesign = Sign(Rad)
+        Rad = valuesign * Rad
+        DEG = Rad * 180 / PI
+        DD = Int(DEG)
+        MM = Int((DEG - DD) * 60)
+        SS = ((DEG - DD) * 60 - MM) * 60
+        SS = String.Format("{0:00.0}", SS)
+
+        If SS >= 60 Then
+            SS -= 60
+            MM += 1
+
+        End If
+        If MM >= 60 Then
+            MM -= 60
+            DD += 1
+
+        End If
+        If valuesign = -1 Then
+            Return String.Format("-{0}°{1}′{2}″", DD, MM, SS)
+        Else
+            Return String.Format("{0}°{1}′{2}″", DD, MM, SS)
+        End If
+    End Function
+    Function Nn(a As Double, B As Double, f As Double) As Double
+        Dim e2 As Double
+        Dim w As Double
+        e2 = 2 * f - f * f
+        w = Sqrt(1 - e2 * Sin(B) ^ 2)
+        Return (a / w)
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        Dim f As Double
+        Dim b1 As Double
+        Dim e1 As Double
+        Dim x1 As Double
+        Dim textout As String = "CGCS2000"
+        If (TextBox_B.TextLength > 0 And TextBox_H.TextLength > 0 And TextBox_L.TextLength > 0) Then
+
+            If (RadioButton1.Checked) Then
+                a = 6378137
+                x1 = 298.257222101
+                b1 = 6356752.31414036
+                e1 = 0.0818191910428081
+                textout = "CGCS2000"
+            ElseIf (RadioButton2.Checked) Then
+                a = 6378137
+                x1 = 298.257223563
+                b1 = 6356752.31424518
+                e1 = 0.0818191908426203
+                textout = "WGS84"
+            ElseIf (RadioButton3.Checked) Then
+                a = 6378140
+                x1 = 298.257
+                b1 = 6356755.28815753
+                e1 = 0.0818192214555198
+                textout = "IUGG75"
+            ElseIf (RadioButton4.Checked) Then
+                a = 6378245
+                x1 = 298.3
+                b1 = 6356863.01877305
+                e1 = 0.081813334016926
+                textout = "Krassovsky"
+            End If
+            f = 1 / x1
+            If RadioButton5.Checked Then
+                Dim B As Double = CRad(TextBox_B.Text)
+                Dim L As Double = CRad(TextBox_L.Text)
+                Dim H As Double = Val(TextBox_H.Text)
+                Dim N As Double = Nn(a, B, f)
+                Dim X, Y, Z As Double
+                X = (N + H) * Cos(B) * Cos(L)
+                Y = (N + H) * Cos(B) * Sin(L)
+                Z = (N * (1 - (2 * f - f * f)) + H) * Sin(B)
+                TextBox4.Text = vbCrLf & "Elipsoid Name: " & textout & vbCrLf
+                TextBox4.Text &= "a=" & a & vbCrLf & "b=" & b1 & vbCrLf
+                TextBox4.Text &= vbCrLf & "f=" & f & vbCrLf & "e=" & e1 & vbCrLf
+                TextBox4.Text &= vbCrLf & "L=" & CMDS(L) & vbCrLf & "B=" & CMDS(B) & vbCrLf & "H=" & H & "M" & vbCrLf
+                TextBox4.Text &= vbCrLf & "X=" & String.Format("{0:0.000}", X) & vbCrLf & "Y=" & String.Format("{0:0.000}", Y) & vbCrLf & "Z=" & String.Format("{0:0.000}", Z) & vbCrLf
+            ElseIf RadioButton6.Checked Then
+                Dim X As Double = TextBox_L.Text
+                Dim Y As Double = TextBox_B.Text
+                Dim Z As Double = TextBox_H.Text
+                Dim e2 As Double = 2 * f - f ^ 2
+                Dim tanB0, tanB1 As Double
+                Dim L, H As Double
+                L = Atan(Y / X)
+                tanB0 = Z / (Sqrt(X ^ 2 + Y ^ 2))
+                tanB1 = (1 / Sqrt(X ^ 2 + Y ^ 2)) * (Z + (a * e2 * tanB0) / (Sqrt(1 + tanB0 ^ 2 - e2 * tanB0 ^ 2)))
+                Do While Abs(tanB1 - tanB0) >= 0.0000001
+                    tanB0 = tanB1
+                    tanB1 = (1 / Sqrt(X ^ 2 + Y ^ 2)) * (Z + (a * e2 * tanB0) / (Sqrt(1 + tanB0 ^ 2 - e2 * tanB0 ^ 2)))
+                Loop
+                Dim B As Double = Atan(tanB1)
+                H = Sqrt(X ^ 2 + Y ^ 2) / Cos(B) - Nn(a, B, f)
+                TextBox4.Text = vbCrLf & "Elipsoid Name: " & textout & vbCrLf
+                TextBox4.Text &= "a=" & a & vbCrLf & "b=" & b1 & vbCrLf
+                TextBox4.Text &= vbCrLf & "f=1/" & x1 & vbCrLf & "e=" & e1 & vbCrLf
+                TextBox4.Text &= vbCrLf & "L=" & CMDS(L + PI) & vbCrLf & "B=" & CMDS(B) & vbCrLf & "H=" & String.Format("{0:0.000}", H) & "M" & vbCrLf
+                TextBox4.Text &= vbCrLf & "X=" & X & vbCrLf & "Y=" & Y & vbCrLf & "Z=" & Z & vbCrLf
+            End If
+        End If
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+        Label2.Text = "L:"
+        Label3.Text = "B:"
+        Label4.Text = "H:"
+        Label5.Text = "D.MMSS"
+        Label6.Text = "D.MMSS"
+    End Sub
+
+    Private Sub RadioButton6_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton6.CheckedChanged
+        Label2.Text = "X:"
+        Label3.Text = "Y:"
+        Label4.Text = "Z:"
+        Label5.Text = "M"
+        Label6.Text = "M"
+    End Sub
+
+    Private Sub TextBox_L_TextChanged(sender As Object, e As EventArgs) Handles TextBox_L.TextChanged
+
+    End Sub
+End Class
